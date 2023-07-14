5b8c5ee94b4aada29f9d192fbf60c4a14c12734c
kimi <kimi0230@gmail.com>
2023-07-14 16:31:05 +0800

    feat: sentiment-analysis

--- pipe1.py
+++ pipe1.py
@@ -4,4 +4,6 @@ classifier = pipeline("sentiment-analysis")
 
 # UserWarning: `return_all_scores` is now deprecated, if want a similar funcionality use `top_k=None` instead of `return_all_scores=True` or `top_k=1` instead of `return_all_scores=False`
 res = classifier("I've been waiting for a HuggingFace course my whole life.", top_k=None)
-print(res)
+
+# [{'label': 'POSITIVE', 'score': 0.9598051905632019}, {'label': 'NEGATIVE', 'score': 0.04019483923912048}]
+print(res) 
